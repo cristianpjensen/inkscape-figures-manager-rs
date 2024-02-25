9298ac02733701a20ced854c6dbd74ea202c6f3e
Cristian Perez Jensen <cristianpjensen@gmail.com>
2024-02-25 21:22:34 +0100

    feat: add file watching functionality for figures directory
    The commit adds support for file watching in the figures directory. This allows the application to detect changes in the figures directory and update the UI accordingly. The `notify` crate is used for this functionality. The commit also includes necessary imports and updates the `New` variant of the `FigureCommand` enum to use the `name` field instead of `file_name`.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ mod clipboard;
 mod shortcuts;
 mod style;
 
+use std::{path::Path, time::Duration};
+
 use clap::{Parser, Subcommand};
 use glob::glob;
 use global_hotkey::{GlobalHotKeyEvent, GlobalHotKeyManager};
+use notify::{poll::ScanEvent, Config, PollWatcher, RecursiveMode, Watcher};
 use tao::event_loop::{ControlFlow, EventLoopBuilder};
 
 #[derive(Parser)]
@@ -27,9 +30,9 @@ enum Commands {
     /// subdirectory
     List,
     /// Creates a new figure for the current document
-    New { file_name: String },
+    New { name: String },
     /// Edits an existing figure for the current document
-    Edit { file_name: String },
+    Edit { name: String },
 }
 
 fn main() {
@@ -37,22 +40,15 @@ fn main() {
 
     match &cli.command {
         Commands::Start => {
+            std::thread::spawn(autosave_pdf_tex);
             hotkeys_listener();
         }
-        Commands::List => {
-            for entry in glob("figures/*.svg").expect("Failed to read glob pattern") {
-                match entry {
-                    Ok(path) => println!("{:?}", path.display()),
-                    Err(e) => println!("{:?}", e),
-                }
-            }
+        Commands::List => list_figures(),
+        Commands::New { name } => {
+            create_figure(name);
+            open_figure(name);
         }
-        Commands::New { file_name } => {
-            println!("New file: {}", file_name);
-        }
-        Commands::Edit { file_name } => {
-            println!("Edit file: {}", file_name);
-        }
+        Commands::Edit { name } => open_figure(name),
     }
 }
 
@@ -71,4 +67,88 @@ fn hotkeys_listener() {
             kbd_shortcuts.handler(event);
         }
     });
+}
+
+fn autosave_pdf_tex() {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let tx_c = tx.clone();
+
+    enum Message {
+        Event(notify::Result<notify::Event>),
+        Scan(ScanEvent),
+    }
+
+    let mut watcher = PollWatcher::with_initial_scan(
+        move |event| {
+            tx_c.send(Message::Event(event)).unwrap();
+        },
+        Config::default().with_poll_interval(Duration::from_secs(1)),
+        move |scan_event| {
+            tx.send(Message::Scan(scan_event)).unwrap();
+        },
+    )
+    .unwrap();
+
+    watcher
+        .watch(Path::new("figures"), RecursiveMode::Recursive)
+        .unwrap();
+
+    for res in rx {
+        match res {
+            Message::Event(e) => {
+                if let Ok(e) = e {
+                    for path in e.paths {
+                        if let Some(filename) = path.file_name() {
+                            let s = filename.to_string_lossy();
+                            if s.ends_with(".svg") {
+                                let output = std::process::Command::new("inkscape")
+                                    .arg("--export-area-page")
+                                    .arg("--export-dpi=300")
+                                    .arg("--export-type=pdf")
+                                    .arg("--export-latex")
+                                    .arg(format!(
+                                        "--export-filename=figures/{}",
+                                        s.replace(".svg", ".pdf")
+                                    ))
+                                    .output();
+
+                                match output {
+                                    Ok(_) => println!("successfully saved {} as pdf", s),
+                                    Err(_) => println!("error: unsuccessfully saved {} as pdf", s),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Message::Scan(e) => {
+                if let Ok(path) = e {
+                    if let Some(filename) = path.file_name() {
+                        let s = filename.to_string_lossy();
+                        if s.ends_with(".svg") {
+                            println!("found figure: {}", s);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn list_figures() {
+    for entry in glob("figures/*.svg").expect("should be able to glob figures/ directory") {
+        if let Ok(path) = entry {
+            if let Some(filename) = path.file_name() {
+                println!("{}", filename.to_string_lossy());
+            }
+        }
+    }
+}
+
+fn create_figure(filename: &str) {
+    println!("Create figure: {}", filename);
+}
+
+fn open_figure(filename: &str) {
+    println!("Open figure: {}", filename);
 }
